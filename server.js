19168424f431cea9cec1c9785c70d6718b664e4c
James Burdick <jamesburdick@Jamess-MacBook-Air.local>
2026-05-11 12:00:51 -0700

    Debug render logging

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const mongodb = require('./db/connect');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+console.log('MONGODB_URI exists?', !!process.env.MONGODB_URI);
 
 app.use(express.json());
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.log('DB connection error:', err);
+    process.exit(1);
   } else {
     app.listen(port, () => console.log(`Connected to DB and listening on ${port}`));
   }
