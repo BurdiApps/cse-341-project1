acaf5c5121f09df2454c76d85721a52cd91ea73a
James Burdick <jamesburdick@Jamess-MacBook-Air.local>
2026-05-11 12:06:21 -0700

    log full error fix

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 8080;
 
-console.log('MONGODB_URI exists?', !!process.env.MONGODB_URI);
-
 app.use(express.json());
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log('DB connection error:', err);
+    console.log('DB connection error:');
+    console.log(err);
     process.exit(1);
   } else {
     app.listen(port, () => console.log(`Connected to DB and listening on ${port}`));
